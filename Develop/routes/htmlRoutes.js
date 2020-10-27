113208941733cf5f9317a4af14fb45b3ede615f5
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-26 20:54:07 -0700

    fixed code

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 var path = require("path");
 
 router.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/stats", function (req, res) {
-    res.sendFile(path.join(__dirname, "stats.html"))
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 module.exports = router;

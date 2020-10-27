358ad5e6927ed0c82de8fd9e53b473db26ed3562
Johnny Lieu <johnnylieu@yahoo.com>
2020-10-26 19:55:56 -0700

    created app.get route for html routes

--- Develop/public/htmlRoutes.js
+++ Develop/public/htmlRoutes.js
@@ -3,5 +3,9 @@ var path = require("path");
 module.exports = function (app) {
     app.get("/exercise", function (req, res) {
         res.sendFile(path.join(__dirname, "exercise.html"));
-    })
+    });
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "index.html"));
+    });
 }
